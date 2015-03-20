b9ec54b53c488a75f1be040a6c98940bcfaac3c4
root <root@ext-mon01-ger.vit.ch>
2015-03-20 08:05:35 +0100

    modules & app

---
+++ app.js
@@ -0,0 +1,40 @@
+var Ping = require('./lib/ping'),
+	
+	websites = [
+
+		{
+
+			url: 'http://vit.picturepark.com/Website/monitoring.aspx',
+			timeout: 5
+
+
+		},
+
+		{
+
+			url: 'http://markant.picturepark.com/Website/monitoring.aspx',
+			timeout: 1
+		}
+
+
+
+	],
+
+	monitors = [];
+
+
+websites.forEach(function (website) {
+
+	var monitor = new Ping ({
+
+		website: website.url,
+		timeout: website.timeout
+
+	});
+
+
+	monitors.push(monitor);
+
+
+
+});
